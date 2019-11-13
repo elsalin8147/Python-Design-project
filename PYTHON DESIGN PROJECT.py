61e4247ebd5d1999aa3c02ea7f083c7c6437b77b
elsalin <elin8147@baysidehighschool.org>
2019-11-13 14:10:42 -0500

    Add files via upload

---
+++ PYTHON DESIGN PROJECT.py
@@ -0,0 +1,124 @@
+#basic shape fundamentals-practice file
+import turtle
+bob=turtle.Turtle()
+#turtle.bgcolor(0,0,0)
+bob.color('pink')
+bob.width(5)
+bob.speed(0)
+for c in range(50):
+  bob.circle(10*c)
+  bob.up()
+  bob.sety((10*c)*(-1))
+  bob.down()
+
+import turtle
+bob=turtle.Turtle()
+bob.width(5)
+bob.speed(0)
+bob.color('red')
+
+bob.right(90)
+bob.forward(80)
+bob.right(180)
+bob.forward(100)
+
+bob.right(90)
+bob.forward(80)
+bob.right(180)
+bob.forward(100)
+
+for times in range(10):
+  bob.left(90)
+  bob.forward(50)
+  bob.left(45)
+  bob.forward(50)
+  
+  bob.right(90)
+  bob.forward(50)
+  bob.right(90)
+  bob.forward(50)
+
+
+import turtle
+bob=turtle.Turtle()
+bob.width(5)
+bob.speed(0)
+bob.color('green')
+
+bob.right(90)
+bob.forward(80)
+bob.right(180)
+bob.forward(100)
+
+for times in range(3):
+  bob.right(90)
+  bob.forward(50)
+  bob.right(45)
+  bob.forward(50)
+
+  bob.left(90)
+  bob.forward(50)
+  bob.left(45)
+  bob.forward(50)
+
+  bob.right(90)
+  bob.forward(50)
+  bob.right(90)
+  bob.forward(50)
+
+import turtle
+bob=turtle.Turtle()
+bob.width(5)
+bob.speed(0)
+bob.color('blue')
+
+bob.left(90)
+bob.forward(160)
+bob.left(180)
+bob.forward(100)
+
+
+for times in range(3):
+  bob.right(90)
+  bob.forward(50)
+  bob.right(45)
+  bob.forward(50)
+
+  bob.left(90)
+  bob.forward(50)
+  bob.left(45)
+  bob.forward(50)
+
+  bob.right(90)
+  bob.forward(50)
+  bob.right(90)
+  bob.forward(50)
+
+import turtle
+bob=turtle.Turtle()
+bob.width(5)
+bob.speed(0)
+bob.color('purple')
+
+bob.right(90)
+bob.forward(80)
+bob.right(180)
+bob.forward(100)
+
+bob.right(90)
+bob.forward(80)
+bob.right(180)
+bob.forward(100)
+
+for times in range(10):
+  bob.right(90)
+  bob.forward(50)
+  bob.right(90)
+  bob.forward(50)
+  
+  bob.left(90)
+  bob.forward(50)
+  bob.left(45)
+  bob.forward(50)
+
+
